f65e7589a601ec6ea35dae4655066a84670b0474
Preveen Padmanabhan <preveen@tekenlight.com>
2025-01-21 14:47:57 +0530

    added a test

---
+++ src/test/java/org/example/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HelloControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void home() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hello() {
+        assert(true);
+    }
+}
